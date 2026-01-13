351b4a4ce497890863dd393548607fa0fdc6370e
Abdullah <abdul.abdelhalim3@gmail.com>
2026-01-13 02:01:31 -0500

    Completed step 5 of the project. Added Psuedocode file

---
+++ Psuedocode.md
@@ -0,0 +1,22 @@
+Psuedocode for writing a function that randomly returns rock, paper, scissors
+
+
+Step 1: Write a function named getCompterChoice - Done
+1. Doesn't need an input since its by the comptuer
+2. Function needs to return randomly "rock", "paper", "scissors"
+3. Can do this with Math.random. Make an array containing the three strings
+4. Will use math.random to randomly select an index from the array
+5. Return the result of math.random (maybe in a variable)
+
+Step 2: Write logic to get human choice - Done
+1. Function needs to take input of user
+2. The only thing in this is that we get the input of player (no comparisons are needed here)
+3. Use "prompt" the get the input of the user
+4. Function does not need parameters. Take user input on function call and return input
+
+Step 3: Declare the players score variable - Done
+1. Create two global variables, one called humanScore and another called computerScore
+2. Initialize those variables to 0
+
+Step 5: Write logic to play a single round - In Progress
+1. Create a function called 
--- rps_javascript.js
+++ rps_javascript.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let comptuerScore = 0;
+
 function getComptuerChoice(){
     let rps = ["rock", "paper", "scissors"];
     let rand = Math.floor(Math.random() * rps.length);
@@ -9,8 +12,31 @@ function getHumanChoice(){
     return choice;
 }
 
+function playRound(a, b){
+    hAns = a.toLowerCase();
+    bAns = b.toLowerCase();
 
+    upA = a.charAt(0).toUpperCase() + a.substring(1, a.length);
+    upB = b.charAt(0).toUpperCase() + b.substring(1, b.length);
 
+    if ((hAns === "rock" &&  bAns === "scissors") || (hAns === "scissors" && bAns === "paper") || (hAns === "paper" && bAns === "rock")){
+        
+        humanScore = humanScore + 1;
+        return "You Won! " + upA + " beats " + upB + "!";
 
+    } else if(hAns === bAns){
+        
+        return "Tie! " + upA + " ties " + upB + "!" ;
 
-    console.log(getHumanChoice())
+    }else{
+        comptuerScore = comptuerScore + 1;
+        return "You Lost! "  + upB + " beats " + upA + "!";
+    }
+
+}
+
+    const playChoice = getHumanChoice();
+    const compChoice = getComptuerChoice();
+
+    console.log(compChoice);
+    console.log(playRound(playChoice, compChoice));
